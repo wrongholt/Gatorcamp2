9a5072c735b9b1f420c081d8571405c643682caf
wrongholt <wrongholt@gmail.com>
2023-04-04 10:30:14 -0500

    3 files modified

--- webpack.config.js
+++ webpack.config.js
@@ -6,28 +6,26 @@ module.exports = {
   devServer: {
     open: 'google-chrome',
     port: 9000,
-    contentBase: [
-      path.join(__dirname, 'src'),
-      path.join(__dirname, 'dist'),
-    ],
+    contentBase: [path.join(__dirname, 'src'), path.join(__dirname, 'dist')],
     watchContentBase: true,
-    liveReload: true
+    liveReload: true,
   },
   module: {
     rules: [
       {
         test: /\.scss$/i,
+        exclude: /node_modules/,
         use: [
           // Creates `style` nodes from JS strings
-          "style-loader",
+          'style-loader',
           // Translates CSS into CommonJS
-          "css-loader",
+          'css-loader',
           // Compiles Sass to CSS
           {
-            loader: "sass-loader",
+            loader: 'sass-loader',
             options: {
               // Prefer `dart-sass`
-              implementation: require.resolve("sass"),
+              implementation: require.resolve('sass'),
             },
           },
         ],
