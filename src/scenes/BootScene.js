80984eb1b087beffd0e1ff103310b709ae4cd7ac
wrongholt <wrongholt@gmail.com>
2023-04-05 16:28:05 -0500

    1 file modified, 4 added

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -5,21 +5,9 @@ class BootScene extends Phaser.Scene {
     super({ key: 'BootScene' });
   }
   preload() {
-    this.load.multiatlas(
-      'counselor1',
-      'https://the-tales-of-old.s3.amazonaws.com/assets/counselor1.json',
-      'https://the-tales-of-old.s3.amazonaws.com/assets'
-    );
-    this.load.multiatlas(
-      'counselor2',
-      'https://the-tales-of-old.s3.amazonaws.com/assets/counselor2.json',
-      'https://the-tales-of-old.s3.amazonaws.com/assets'
-    );
-    this.load.multiatlas(
-      'counselor3',
-      'https://the-tales-of-old.s3.amazonaws.com/assets/counselor3.json',
-      'https://the-tales-of-old.s3.amazonaws.com/assets'
-    );
+    this.load.multiatlas('counselor1', 'assets/counselor1.json', 'assets');
+    this.load.multiatlas('counselor2', 'assets/counselor2.json', 'assets');
+    this.load.multiatlas('counselor3', 'assets/counselor3.json', 'assets');
   }
 
   create() {
