ccc1e301db18ff48a1b17143080c99d103a5ab00
wrongholt <wrongholt@gmail.com>
2023-04-10 11:36:27 -0500

    1 file modified, 1 added

--- src/scenes/GatorCampScene.js
+++ src/scenes/GatorCampScene.js
@@ -13,7 +13,63 @@ class GatorCampScene extends Phaser.Scene {
     var newLeftChar;
     var newHeight = this.game.config.height;
     var newWidth = this.game.config.width;
+    var mainCharacter = this.characterId;
+    var topContainer = this.add.container(0, 0);
+    var speechContainer = this.add.container(250, 380);
+    topContainer.setSize(newWidth, newHeight * 0.15);
+    var bubbleWidth = 200;
+    var bubbleHeight = 200;
+    var bubblePadding = 10;
+    var arrowHeight = bubbleHeight / 4;
 
+    var bubble = this.add.graphics({ x: 0, y: 0 });
+    var quote = '';
+    //  Bubble shadow
+    bubble.fillStyle(0x222222, 0.5);
+    bubble.fillRoundedRect(6, 6, bubbleWidth, bubbleHeight, 16);
+
+    //  Bubble color
+    bubble.fillStyle(0xffffff, 1);
+
+    //  Bubble outline line style
+    bubble.lineStyle(4, 0x565656, 1);
+
+    //  Bubble shape and outline
+    bubble.strokeRoundedRect(0, 0, bubbleWidth, bubbleHeight, 16);
+    bubble.fillRoundedRect(0, 0, bubbleWidth, bubbleHeight, 16);
+
+    //  Calculate arrow coordinates
+    var point1X = Math.floor(bubbleWidth / 7);
+    var point1Y = bubbleHeight;
+    var point2X = Math.floor((bubbleWidth / 7) * 2);
+    var point2Y = bubbleHeight;
+    var point3X = Math.floor(bubbleWidth / 7);
+    var point3Y = Math.floor(bubbleHeight + arrowHeight);
+
+    //  Bubble arrow shadow
+    bubble.lineStyle(4, 0x222222, 0.5);
+    bubble.lineBetween(point2X - 1, point2Y + 6, point3X + 2, point3Y);
+
+    //  Bubble arrow fill
+    bubble.fillTriangle(point1X, point1Y, point2X, point2Y, point3X, point3Y);
+    bubble.lineStyle(2, 0x565656, 1);
+    bubble.lineBetween(point2X, point2Y, point3X, point3Y);
+    bubble.lineBetween(point1X, point1Y, point3X, point3Y);
+
+    var content = this.add.text(0, 0, quote, {
+      fontFamily: 'Arial',
+      fontSize: 20,
+      color: '#000000',
+      align: 'center',
+      wordWrap: { width: bubbleWidth - bubblePadding * 2 },
+    });
+
+    var b = content.getBounds();
+
+    content.setPosition(bubble.x + 15, bubble.y + 15);
+    speechContainer.add(bubble);
+    speechContainer.add(content);
+    speechContainer.setVisible(false);
     var backButton = this.add.image(1, 1, 'back');
     backButton.setOrigin(-0.02, -0.02);
     backButton.setScale(0.4);
@@ -33,16 +89,13 @@ class GatorCampScene extends Phaser.Scene {
     bg.setScale(newWidth / 1920);
     bg.setOrigin(0, 0.04);
     bg.setTint(11843512);
-    var headingImage = this.add.image(newWidth * 0.5, -500, 'gatorHeading');
+    var headingImage = this.add.image(
+      newWidth * 0.5,
+      newHeight * 0.1,
+      'gatorHeading'
+    );
     headingImage.setOrigin(0.5);
     headingImage.setScale(newWidth / 4000);
-    this.tweens.add({
-      targets: headingImage,
-      y: newHeight * 0.1,
-      ease: 'Bounce.easeInOut',
-      delay: 0,
-      duration: 1000,
-    });
     var owl = this.add
       .image(-450, -390, 'owl')
       .setBlendMode(Phaser.BlendModes.NORMAL)
@@ -67,11 +120,39 @@ class GatorCampScene extends Phaser.Scene {
       .setInteractive();
     gator.setScale(newWidth / 3800);
     gator.setOrigin(0.5, 0.5);
+    // speechContainer.add(speech);
     gator.on('pointerover', function (pointer) {
+      // speech.setVisible(true);
+      quote =
+        '"We have tons of food and the choices are endless(not really): Chicken or Kale."';
+      speechContainer.setVisible(true);
+      content.setText(quote);
       console.log('Hello');
     });
-    gator.on('pointerout', function (pointer) {});
-    var mainCharacter = this.characterId;
+    gator.on('pointerout', function (pointer) {
+      speechContainer.setVisible(false);
+    });
+    bear.on('pointerover', function (pointer) {
+      // speech.setVisible(true);
+      quote =
+        '"Be careful where you go you never know what is lurking in the shadows."';
+      speechContainer.setVisible(true);
+      content.setText(quote);
+      console.log('Hello');
+    });
+    bear.on('pointerout', function (pointer) {
+      speechContainer.setVisible(false);
+    });
+    owl.on('pointerover', function (pointer) {
+      // speech.setVisible(true);
+      quote = '"There are tons of animals and plants to see."';
+      speechContainer.setVisible(true);
+      content.setText(quote);
+      console.log('Hello');
+    });
+    owl.on('pointerout', function (pointer) {
+      speechContainer.setVisible(false);
+    });
     console.log('THE NEW CHAR---->>>>', mainCharacter);
     newLeftChar = this.add
       .sprite(1, 0.5, mainCharacter, `${mainCharacter}Idle_000.png`)
@@ -136,8 +217,7 @@ class GatorCampScene extends Phaser.Scene {
 
       spotlight.setColor(colors[currentColor]);
     });
-    var topContainer = this.add.container(0, 0);
-    topContainer.setSize(newWidth, newHeight * 0.15);
+
     var campContainer = this.add.container(0, newHeight * 0.2);
     campContainer.setSize(newWidth, newHeight * 0.85);
     var newLeftContainer = this.add.container(newWidth / 6 - 10, newHeight / 2);
